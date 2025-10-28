3f7e19cb507de855ee56241c5c01c3fc5f7b78d5
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-10-28 14:34:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
+let quoteLength = quote.length;
+let twentySecondLetter = quote[21];
+let output = `the length of the quote is ${quoteLength} and the twenty-second letter in the quote is ${twentySecondLetter}`;
+
+document.getElementById("result").innerHTML = output;
